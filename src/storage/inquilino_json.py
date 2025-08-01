69e8da2106421d694e84e76d9e976da9eadd090d
jrdimande <timoteojrdimande@gmail.com>
2025-08-01 23:50:33 +0200

    feat: criação de caminhos

--- src/storage/inquilino_json.py
+++ src/storage/inquilino_json.py
@@ -1,37 +1,44 @@
 import json
+import os
 from src.controllers.InquilinoController import InquilinoController
 
-def dump(inc):
-    """ Salvar os dados no arquivo .json """
-    InquilinoController = inc
-    filename = "inquilinos.json"
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+FILENAME = os.path.join(BASE_DIR, "storage", "inquilinos.json")
 
-    dados = {"inquilinos" : []}
+def dump(inc):
+    "Salvar dados no arquivo json"
+    InquilinoController = inc
+
+    dados = {"inquilinos": []}
 
     for inquilino in InquilinoController.inquilinos:
-        actual = {"id" : inquilino.id,
-                  "nome" : inquilino.nome,
-                  "contacto" : inquilino.contacto,
-                  "data_de_entrada" : inquilino.data_de_entrada,
-                  "imovel" : inquilino.imovel,
-                  "pagamentos" : inquilino.pagamentos
-                  }
+        actual = {
+            "id": inquilino.id,
+            "nome": inquilino.nome,
+            "contacto": inquilino.contacto,
+            "data_de_entrada": inquilino.data_de_entrada,
+            "imovel": inquilino.imovel,
+            "pagamentos": inquilino.pagamentos
+        }
         dados["inquilinos"].append(actual)
 
-    with open(filename, "w") as f:
+    os.makedirs(os.path.dirname(FILENAME), exist_ok=True)
+
+    with open(FILENAME, "w", encoding="utf-8") as f:
         json.dump(dados, f, indent=4)
 
 def load():
-    """ Carregar os dados no arquivo .json """
-    filename = "inquilinos.json"
+    "Carregar dados no arauivo json"
+    os.makedirs(os.path.dirname(FILENAME), exist_ok=True)
+
     try:
-        with open(filename, "r") as f:
+        with open(FILENAME, "r", encoding="utf-8") as f:
             inquilinos = json.load(f)
         return inquilinos
     except FileNotFoundError:
         return {"inquilinos": []}
 
 def open_and_dump(data):
-    filepath = "inquilinos.json"
-    with open(filepath, "w") as f:
+    os.makedirs(os.path.dirname(FILENAME), exist_ok=True)
+    with open(FILENAME, "w", encoding="utf-8") as f:
         json.dump(data, f, indent=4)
